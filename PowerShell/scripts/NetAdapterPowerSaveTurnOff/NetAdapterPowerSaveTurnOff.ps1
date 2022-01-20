a6866f1afc83dcaa15bc13322864c283b609ea02
Byungho Park <bhobak@gmail.com>
2022-01-20 14:24:21 +0900

    Update NetAdapterPowerSaveTurnOff.ps1

--- PowerShell/scripts/NetAdapterPowerSaveTurnOff/NetAdapterPowerSaveTurnOff.ps1
+++ PowerShell/scripts/NetAdapterPowerSaveTurnOff/NetAdapterPowerSaveTurnOff.ps1
@@ -1,3 +1,6 @@
+# Turn Off Power Saving Function of All Network Adapters
+# A simple script running on PowerShell in Windows 10+
+
 $adapters = Get-NetAdapter -Physical | Get-NetAdapterPowerManagement
 
 foreach ($adapter in $adapters)
